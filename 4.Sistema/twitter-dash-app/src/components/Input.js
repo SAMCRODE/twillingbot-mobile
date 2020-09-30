ccfb9c40c9cb1c089ab57955c631a779f4a7a61d
rafaelcsva <rafaelgyncastro63@gmail.com>
2020-09-30 20:52:23 -0300

    Login screen validation; Taking away expo-constant dep

--- 4.Sistema/twitter-dash-app/src/components/Input.js
+++ 4.Sistema/twitter-dash-app/src/components/Input.js
@@ -1,9 +1,68 @@
-import React from 'react';
-import { TextInput, StyleSheet, View } from 'react-native';
+import React, { useReducer, useEffect } from 'react';
+import { TextInput, StyleSheet, View, Text } from 'react-native';
 
 import { FontAwesome } from '@expo/vector-icons';
 
+const INPUT_CHANGE = 'INPUT_CHANGE';
+const INPUT_BLUR = 'INPUT_BLUR';
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case INPUT_CHANGE:
+      return {
+        ...state,
+        value: action.value,
+        isValid: action.isValid,
+        touched: true
+      };
+    case INPUT_BLUR:
+      return {
+        ...state,
+        touched: true
+      };
+    default:
+      return state;
+  }
+};
+
 const Input = props => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: props.initialValue ? props.initialValue : '',
+    isValid: !!props.initiallyValid,
+    touched: false
+  });
+
+  const { onInputChange, id } = props;
+
+  useEffect(() => {
+    // console.log('vou chamar ? ', inputState.touched)
+    if (inputState.touched) {
+      onInputChange(id, inputState.value, inputState.isValid);
+    }
+  }, [inputState, onInputChange, id]);
+
+  const textChangeHandler = text => {
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let isValid = true;
+    // console.log(text, 'eh o texto')
+    if (props.required && text.trim().length === 0) {
+      isValid = false;
+    }
+    if (props.email && !emailRegex.test(text.toLowerCase())) {
+      isValid = false;
+    }
+    if (props.minLength != null && text.length < props.minLength) {
+      isValid = false;
+    }
+    // console.log(isValid, 
+    dispatch({ type: INPUT_CHANGE, value: text, isValid: isValid });
+  };
+
+  const lostFocusHandler = () => {
+    // console.log('ON BLUR')
+    dispatch({ type: INPUT_BLUR });
+  };
+
   let icon;
 
   if(props.icon){
@@ -17,7 +76,14 @@ const Input = props => {
       {...props} 
       style={{ ...styles.input, ...props.style }} 
       placeholder={props.placeholder}
+      onChangeText={textChangeHandler}
+      onEndEditing={lostFocusHandler}
     />
+    {/* {!inputState.isValid && inputState.touched && (
+      <View style={styles.errorContainer}>
+        <Text style={styles.errorText}>{props.errorText}</Text>
+      </View>
+    )} */}
   </View>);
 };
 
@@ -35,6 +101,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  errorContainer: {
+    marginVertical: 5
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 13
   }
 });
 
