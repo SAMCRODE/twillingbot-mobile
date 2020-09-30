a5f053ccdf7eeab97e51c6afece20a56539a8376
Matheus van der Lemon <imlearningcodes@gmail.com>
2020-09-30 14:49:42 -0300

    Create a tweet screen

--- 4.Sistema/twitter-dash-app/src/screens/Tweet/TweetScreen.js
+++ 4.Sistema/twitter-dash-app/src/screens/Tweet/TweetScreen.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { TextInput } from 'react-native-paper';
 import styles from './styles';
+import logoImg from '../assets/logo.png';
+
 const TweetScreen = props => {
+
+
   return (
     <View style={styles.screen}>
-      <Text>The Tweet Screen</Text>
+
+
+      <Image style={styles.imagem} source={logoImg}/>
+
+      <View style={styles.inputContainer}>
+      <Text style={styles.title}>O que você está pensando?</Text>  
+      <TextInput 
+      style={styles.input}
+      multiline = {true}
+      numberOfLines = {3}
+      maxLength={240}
+      onFocus={false}
+      autoCapitalize="none"
+      autoCorrect={false}
+      theme={{colors: {primary: '#1DA1F2', underlineColor: 'transparent'}}}
+      />
+      <View style={styles.actions} >
+        <TouchableOpacity style={styles.buttonTweet} onPress={() =>{}}>
+          <Text style={styles.buttonText}>Tweetar</Text>
+        </TouchableOpacity>
+      </View>
+      </View>
+    	
+
     </View>
   );
 };
 
 
 export default TweetScreen;
+[ ]
--- 4.Sistema/twitter-dash-app/src/screens/Tweet/styles.js
+++ 4.Sistema/twitter-dash-app/src/screens/Tweet/styles.js
@@ -1,9 +1,42 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 
 export default StyleSheet.create({
-    screen: {
+    screen:{
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'   
-     }
+        alignItems: 'center',
+        backgroundColor: '#F5F8FA',
+        paddingTop: 27
+     },
+     imagem:{
+         marginBottom: 25
+     },
+      title:{
+        fontSize: 15,
+        marginBottom: 5
+      },
+      inputContainer:{
+        marginTop: 25
+      },
+      input: {
+        width: Dimensions.get('window').width * 0.80,
+        backgroundColor: "#E1E8ED",
+        maxHeight: 95
+      },
+      actions:{
+        flexDirection: 'row-reverse'
+      },
+      buttonTweet: {
+        width: 105,
+        height: 35,
+        marginTop: 10,
+        backgroundColor: '#1DA1F2',
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      buttonText:{
+        color: '#fff',
+        fontWeight: 'bold'
+      }
 })
