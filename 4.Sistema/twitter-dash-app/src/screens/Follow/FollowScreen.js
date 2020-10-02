2988c2605d4ab9bce37b1fa2bd620cdcc35282f5
Matheus van der Lemon <imlearningcodes@gmail.com>
2020-10-02 18:15:23 -0300

    Create follow screen

--- 4.Sistema/twitter-dash-app/src/screens/Follow/FollowScreen.js
+++ 4.Sistema/twitter-dash-app/src/screens/Follow/FollowScreen.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import styles from './styles';
+import logoImg from '../assets/logo.png';
+import Input from '../../components/Input';
 
 const FollowScreen = props => {
   return (
     <View style={styles.screen}>
-      <Text>The Follow Screen</Text>
+    <Image style={styles.imagem} source={logoImg}/>
+
+
+    <View style={styles.inputContainer}>
+
+        <Text style={styles.title}>Digite o nome do usuário:</Text>
+
+        <Input 
+        style={styles.input}
+        blurOnSubmit
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Usuário"
+        />
+
+        <View style={styles.actions} >
+          <TouchableOpacity style={styles.buttonTweet} onPress={() =>{}}>
+            <Text style={styles.buttonText}>Seguir</Text>
+          </TouchableOpacity>
+        </View>
+
+    </View>
+
+
     </View>
   );
 };
