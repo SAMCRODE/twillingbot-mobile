d33b64fd0ce85cd079a8e1b0b76d08ea439f276c
rafaelcsva <rafaelgyncastro63@gmail.com>
2021-01-10 21:52:28 -0300

    Add eslint config

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+  },
 };
